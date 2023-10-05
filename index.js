31d97a7bac81e102c902245ee82d14f9320ef8f0
Samantha Brown <113550193+Samantha-Source@users.noreply.github.com>
2023-10-05 02:12:36 -0400

    Update index.js

--- index.js
+++ index.js
@@ -22,9 +22,6 @@ app.get('/', function (req, res) {
 // your first API endpoint...
 app.get('/api/whoami', function (req, res) {
   const { host, ['accept-language']: userLanguage, ['user-agent']: userSoftware } = req.headers;
-  // const userLanguage = req.headers['accept-language'];
-  // const userSoftware = req.headers['user-agent'];
-
   res.json({ ipaddress: host, language: userLanguage, software: userSoftware });
 });
 
